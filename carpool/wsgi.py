a8b8e4af96c1fc5d12a9ec074a1bd48952d2bd1a
Daniel Domingo-Fernández <danieldomingofernandez@hotmail.com>
2019-07-23 10:47:32 +0200

    Update wsgi.py

--- carpool/wsgi.py
+++ carpool/wsgi.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 
-"""WSGI config for the CarPool web application.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
 import os
-
+from django.core.wsgi import get_wsgi_application
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carpool.settings")
 django.setup()
+
+application = get_wsgi_application()
